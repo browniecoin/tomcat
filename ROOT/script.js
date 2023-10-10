de88addf6b7b4615a8de4cbe80f1a58bed40006b
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-09 20:54:47 -0700

    update

--- ROOT/script.js
+++ ROOT/script.js
@@ -1,3 +1,17 @@
+// Define the URL to fetch JSON data
+const url = 'https://browniecoins.org/home/coin_stats/';
+
+// Function to fetch JSON data from the URL
+async function fetchData() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data; // The data should be an array of objects
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
 // Function to create and update the line chart
 function createLineChart(data) {
     const timestamps = data.map(entry => new Date(entry.fields.timestamp));
