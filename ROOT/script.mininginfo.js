39e8f312bad0d6340065ebde82ffa474b80e1c20
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-18 00:22:03 -0700

    update

--- ROOT/script.mininginfo.js
+++ ROOT/script.mininginfo.js
@@ -1,10 +1,10 @@
 // Define the URL to fetch JSON data
-const url_coin_supply = 'https://browniecoins.org/home/coin_stats/';
+const url_mining_supply = 'https://browniecoins.org/home/coin_stats/';
 
 // Function to fetch JSON data from the URL
 async function fetchMiningData() {
     try {
-        const response = await fetch(url_coin_supply);
+        const response = await fetch(url_mining_supply);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
